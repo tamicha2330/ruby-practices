700cac7a24d879d6366077c524d9c16af689f851
tamicha2330 <winter483@icloud.com>
2023-06-02 20:13:45 +0900

    "インデントを調整" "FizzとBuzzの頭文字を大文字に変更"

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,12 +1,12 @@
 (1..20).each do |x|
-    case
-    when x % 3 == 0 && x % 5 == 0
-        puts "fizzbuzz"
-    when x % 3 == 0
-        puts "fizz"
-    when x % 5 == 0
-        puts "buzz"
-    else
-        puts x
-    end
+  case
+  when x % 3 == 0 && x % 5 == 0
+		puts "FizzBuzz"
+	when x % 3 == 0
+		puts "Fizz"
+	when x % 5 == 0
+		puts "Buzz"
+	else
+		puts x
+	end
 end    
