ce0eeff148b46c967e6f3a8fc24d947c1204322d
tamicha2330 <winter483@icloud.com>
2023-06-22 12:28:39 +0900

    再提出３回目
    if文を簡潔に記述
    週のデータを直接each分で出力できるように変更
    空白を定数にする

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -14,20 +14,17 @@ month = input_month.zero? ? today.month : input_month
 #年月を表示する
 print "#{month}月 #{year}\n".rjust(15)
 
-#週のデータ
-week_days = ["日","月","火","水","木","金","土"]
 #週を表示する
-week_days.each {|day| print day.rjust(2)}
+["日","月","火","水","木","金","土"].each {|day| print day.rjust(2)}
 puts
 
 #初日の曜日に合わせて、空白を表示する
-print " ".rjust(3) * Date.new(year,month,1).wday
+Blank = " " * 3 
+print Blank * Date.new(year,month,1).wday
 #日付を表示する
 (Date.new(year,month,1)..Date.new(year,month,-1)).each do |day|
   print day.day.to_s.rjust(3)
-  if day.saturday? == true
-    puts
-  end
+  puts if day.saturday?
 end
 puts
 
