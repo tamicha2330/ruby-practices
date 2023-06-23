9915e73633cb143664bf1f26040588b3e3cd4402
tamicha2330 <winter483@icloud.com>
2023-06-23 20:22:54 +0900

    カレンダーのプログラム　再提出４回目
    定数を全て大文字にする
    コメントを整理

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -1,27 +1,23 @@
 require "optparse"
 require "date"
-#現在時刻のデータ
+#現在時刻のデータを設定
 today = Date.today
-#引数が与えられた時のデータ
+
 input_time =  OptionParser.new.getopts("y:","m:")
 input_year = input_time["y"].to_i
 input_month = input_time["m"].to_i
 
-#表示する年のデータ
 year = input_year.zero? ? today.year : input_year
-#表示する月のデータ
 month = input_month.zero? ? today.month : input_month
-#年月を表示する
 print "#{month}月 #{year}\n".rjust(15)
 
-#週を表示する
 ["日","月","火","水","木","金","土"].each {|day| print day.rjust(2)}
 puts
 
-#初日の曜日に合わせて、空白を表示する
-Blank = " " * 3 
-print Blank * Date.new(year,month,1).wday
-#日付を表示する
+#空白を作るための定数を設定
+BLANK = " " * 3 
+
+print BLANK * Date.new(year,month,1).wday
 (Date.new(year,month,1)..Date.new(year,month,-1)).each do |day|
   print day.day.to_s.rjust(3)
   puts if day.saturday?
